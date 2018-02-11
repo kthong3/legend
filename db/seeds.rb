3d5a51edba211fa040d0f613b8bb4b564cc61560
kthong3 <kthongsakounh@gmail.com>
2018-02-10 23:58:59 -0600

    add Map view and faker gem for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do Map.create({
+  title: Faker::GameOfThrones.city,
+  description: Faker::HowIMetYourMother.catch_phrase
+  })
+end
