78d0c73455ac4324234b00ce427534f648b7231b
kthong3 <kthongsakounh@gmail.com>
2018-02-15 16:58:26 -0600

    add pending tests for initialize method

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,2 +1,19 @@
 require 'rails_helper'
 
+describe User do
+  let(:user) { User.new("jon", "snow", "iknownothing@email.com", "password")}
+
+
+  context "#initialize" do
+    it "creates a User object" do
+      expect(user).to be_an_instance_of User
+    end
+
+    it "is valid with valid attributes"
+    it "is not valid without a first name"
+    it "is not valid without a last name"
+    it "is not valid without an email address"
+    it "is not valid without a password"
+  end
+
+end
