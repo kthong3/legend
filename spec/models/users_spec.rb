59602a49c7e39c7f2c2502cd081dba69af3437ba
kthong3 <kthongsakounh@gmail.com>
2018-02-20 15:39:06 -0600

    add tests for valid attributes, validation for first_name and last_name

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,17 +1,33 @@
 require 'rails_helper'
 
 describe User do
-  let(:user) { User.new("jon", "snow", "iknownothing@email.com", "password")}
+  let(:user) { User.new }
 
-
-  context "#initialize" do
-    it "creates a User object" do
-      expect(user).to be_an_instance_of User
+  describe "validations" do
+    it "is valid with valid attributes" do
+      user.first_name = "jon"
+      user.last_name = "snow"
+      user.email = "iknownothing@email.com"
+      user.password = "password"
+      user.valid?
+      expect(user.errors).to be_empty
     end
 
-    it "is valid with valid attributes"
-    it "is not valid without a first name"
-    it "is not valid without a last name"
+    it "is not valid without a first name" do
+      user.last_name = "snow"
+      user.email = "iknownothing@email.com"
+      user.password = "password"
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without a last name" do
+      user.first_name = "jon"
+      user.email = "iknownothing@email.com"
+      user.password = "password"
+      expect(user).to_not be_valid
+    end
+
+
     it "is not valid without an email address"
     it "is not valid without a password"
   end
